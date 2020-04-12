efa2cb20b0884e24d77bc166a644a6b1595809e6
YRlearner <35609645+YRlearner@users.noreply.github.com>
2020-04-13 00:46:50 +0100

    creat chall2

---
+++ chall2.py
@@ -0,0 +1,6 @@
+print('calcul area squard')
+print('hello user')
+width=int(input('enter the width : '))
+height=int(input('enter the height : '))
+area = width * height
+print("area is "+ str(area))
